7f9a55071906f76c16ef3f9e44c834f993ae4880
Code em um minuto <alanlucascode@gmail.com>
2023-04-22 18:27:46 -0300

    Python orientado a objetos aulas

---
+++ Conceitos e Programas-Exemplos/classe_atributo.py
@@ -0,0 +1,15 @@
+# Declarando e instanciando classes
+
+class Carro:
+    cor = "Vermelho"
+    marca = "Ford"
+
+'''
+carro1 = Carro()
+print(carro1.cor)
+print(carro1.marca)
+'''
+
+carro2 = Carro()
+print(carro2.cor)
+print(carro2.marca)
---
+++ Conceitos e Programas-Exemplos/metodos_self.py
@@ -0,0 +1,10 @@
+class Carro:
+    cor = "Azul"
+
+    def ligar(self):
+        print("Ligando o carro...")
+
+
+carro1 = Carro()
+print(carro1.cor)
+carro1.ligar()
